53011213c417683f867a591bce9a50916f0d47f3
Thana31 <havret.francois@gmail.com>
2017-01-04 13:58:15 +0100

    PlayList.java
    ajout des mots clefs

--- src/principal/Playlist.java
+++ src/principal/Playlist.java
@@ -7,10 +7,12 @@ public class Playlist {
 	private String nom;
 	private List<Musique> musiques;
 	private List<Utilisateur> utilisateurs;
+	private List<String> motsClefs;
 	
 	public Playlist() {
 		this.musiques = new ArrayList<Musique>();
 		this.utilisateurs = new ArrayList<Utilisateur>();
+		this.motsClefs = new ArrayList<String>();
 	}
 
 	public String getNom() {
@@ -29,11 +31,19 @@ public class Playlist {
 		return utilisateurs;
 	}
 
+	public List<String> getMotsClefs() {
+		return this.motsClefs;	
+	}
+	
 	public void setMusiques(List<Musique> musiques) {
 		this.musiques = musiques;
 	}
 
 	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
 		this.utilisateurs = utilisateurs;
+	}
+	
+	public void setMotsClefs(List<String> motsClefs) {
+		this.motsClefs = motsClefs;	
 	}
 }
