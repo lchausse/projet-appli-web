ed8c41b0bac86da73c43554464c23c52e44df6ca
Chausserie-lapree Louis <lchausse@localhost.localdomain>
2017-01-04 13:30:17 +0100

    maj de Utilisateur.java

--- src/principal/Utilisateur.java
+++ src/principal/Utilisateur.java
@@ -1,7 +1,5 @@
 package principal;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -10,9 +8,6 @@ public class Utilisateur {
 	@Id
 	private String pseudo;
 	private String mdp;
-	private String mail;
-	private String prenom;
-	private String nom;
 	
 	public Utilisateur() {
 	}
@@ -31,29 +26,5 @@ public class Utilisateur {
 	
 	public void setMdp(String mdp) {
 		this.mdp = mdp;
-	}
-	
-	public String getMail() {
-		return mail;
-	}
-	
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-	
-	public String getPrenom() {
-		return prenom;
-	}
-	
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-	
-	public void setNom(String nom) {
-		this.nom = nom;
 	}
 }
