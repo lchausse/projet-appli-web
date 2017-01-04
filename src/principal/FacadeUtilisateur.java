de75278d8e35089b961d73a53c897078b6e92d53
Chausserie-lapree Louis <lchausse@localhost.localdomain>
2017-01-04 16:33:40 +0100

    Ajout de seConnecter

--- src/principal/FacadeUtilisateur.java
+++ src/principal/FacadeUtilisateur.java
@@ -18,38 +18,60 @@ public class FacadeUtilisateur {
 		this.utilisateurs = new ArrayList<Utilisateur>();
 	}
 	
-	public void ajouterUtilisateur(String pseudo, String mdp) {
-		Utilisateur user = new Utilisateur();
-		user.setPseudo(pseudo);
-		user.setMdp(mdp);
-		em.persist(user);
+	public boolean ajouterUtilisateur(String pseudo, String mdp, String confirmationMdp) {
+		boolean compteCree = false;
+		if (mdp == confirmationMdp && getUtilisateur(pseudo) == null ) {
+			Utilisateur user = new Utilisateur();
+			user.setPseudo(pseudo);
+			user.setMdp(mdp);
+			em.persist(user);
+			compteCree = true;
+		}
+		return compteCree;
 	}
-	
+
 	public void modifierPseudo(String ancienPseudo, String nouveauPseudo) {
 		this.getUtilisateur(ancienPseudo).setPseudo(nouveauPseudo);
 	}
-	
+
 	public void modifierMdp(String pseudo, String mdp) {
 		this.getUtilisateur(pseudo).setMdp(mdp);
 	}
-	
+
 	public void partagerPlayList() {
-		
 	}
-	
+
 	public void ajouterPlayList() {
 	}
-	
-	public void seConnecter() {
+
+	public boolean seConnecter(String pseudo, String mdp) {
+		boolean res;
+		TypedQuery<Utilisateur> req = em.createQuery(
+				"SELECT u FROM Utilisateur u WHERE u.pseudo LIKE :pseudo2", Utilisateur.class)
+				.setParameter("pseudo2", pseudo);
+		if (req.getResultList().size() == 0) {
+			res = false;
+		} else {
+			Utilisateur user = req.getResultList().get(0);
+			if (mdp.equals(user.getMdp())) {
+				res = true;
+			} else {
+				res = false;
+			}
+		}
+		return res;
 	}
-	
+
 	public Utilisateur getUtilisateur(String pseudo2) {
 		TypedQuery<Utilisateur> req = em.createQuery(
 				"SELECT u FROM Utilisateur u WHERE u.pseudo LIKE :pseudo2", Utilisateur.class)
 				.setParameter("pseudo2", pseudo2);
 		
-		return req.getResultList().get(0);
+		if (req.getResultList().size() == 0) {
+			return null;	
+		}
+		else {
+			return req.getResultList().get(0);
+		}
 	}
-	
-	
 }
