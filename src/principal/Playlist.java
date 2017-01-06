b6d02ba22466772a918d2bc038915eef7c2c8627
Thana31 <havret.francois@gmail.com>
2017-01-06 18:42:43 +0100

    Update Playlist.java

--- src/principal/Playlist.java
+++ src/principal/Playlist.java
@@ -15,17 +15,17 @@ public class Playlist {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	private String nom;
-//	private List<Musique> musiques;
-	@ManyToMany(fetch=FetchType.EAGER)
-	private List<Utilisateur> utilisateurs;
-//	private List<String> motsClefs;
-	private int vues;
+	private String nom; // nom de la playlist
+	private List<Musique> musiques; // musique de la playlist
+	@ManyToMany(fetch=FetchType.EAGER) 
+	private List<Utilisateur> utilisateurs; // utilisateur pouvant modifier la playlist
+	private List<String> motsClefs; // mots clefs de la playlist
+	private int vues; // nombre de vue de la playlistre
 	
 	public Playlist() {
-//		this.musiques = new ArrayList<Musique>();
+		this.musiques = new ArrayList<Musique>();
 		this.utilisateurs = new ArrayList<Utilisateur>();
-//		this.motsClefs = new ArrayList<String>();
+		this.motsClefs = new ArrayList<String>();
 		this.vues = 0;
 	}
 
@@ -36,6 +36,54 @@ public class Playlist {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	public List<Musique> getMusiques() {
+		return musiques;
+	}
+
+	public List<Utilisateur> getUtilisateurs() {
+		return utilisateurs;
+	}
+
+	
+	
+	public List<Musique> getMusiques() {
+		return this.musiques;
+	}
+	
+	public void setMusiques(List<Musique> musiques) {
+		this.musiques = musiques;
+	}
+	
+	public void addMusique(Musique musique) {
+		this.musiques.add(musique);	
+	}
+	
+	public void deleteMusique(Musique musique) {
+		this.musiques.remove(musique);
+	}
+
+	
+	public List<Utilisateur> getUtilisateurs() {
+		return this.utilisateurs;
+	}
+	
+	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
+		this.utilisateurs = utilisateurs;
+	}
+	
+	public void addUtilisateur(Utilisateur user) {
+		this.utilisateurs.add(user);
+	}
+
+	
+	public List<String> getMotsClefs() {
+		return this.motsClefs;	
+	}
+	public void setMotsClefs(List<String> motsClefs) {
+		this.motsClefs = motsClefs;	
+	}
+
 
 	public int getVues() {
 		return this.vues;
@@ -45,32 +93,7 @@ public class Playlist {
 		this.vues = vues;	
 	}
 	
-//	public List<Musique> getMusiques() {
-//		return musiques;
-//	}
-
-	public List<Utilisateur> getUtilisateurs() {
-		return utilisateurs;
+	public void addVues() {
+		this.vues++;
 	}
-
-//	public List<String> getMotsClefs() {
-//		return this.motsClefs;	
-//	}
-	
-//	public void setMusiques(List<Musique> musiques) {
-//		this.musiques = musiques;
-//	}
-
-	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
-		this.utilisateurs = utilisateurs;
-	}
-
-	public void setMotsClefs(List<String> motsClefs) {
-//		this.motsClefs = motsClefs;	
-	}
-	
-	public void addUtilisateur(Utilisateur user) {
-		this.utilisateurs.add(user);
-	}
-
 }
